9573a1afea82414bd0a258accdbb68253feaab07
desenvolvedor2-eduardo <desenvolvimento2@gmobil.com.br>
2024-07-12 07:51:27 -0300

    last class
    add routes and validations

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,51 +1,44 @@
-import { env } from '@/env'
-import { dayjs } from '@/lib/dayjs'
-import { prisma } from '@/lib/prisma'
-import { getMailClient } from '@/mail'
-import nodemailer from 'nodemailer'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
-import z from 'zod'
+import nodemailer from 'nodemailer'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+import { getMailClient } from '../lib/mail'
+import { dayjs } from '../lib/dayjs'
+import { ClientError } from '../errors/client-error'
+import { env } from '../env'
 
-export const createTrip = async (app: FastifyInstance) => {
+export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/trips',
     {
       schema: {
-        tags: ['trips'],
-        summary: 'Create a new trip',
         body: z.object({
           destination: z.string().min(4),
           starts_at: z.coerce.date(),
           ends_at: z.coerce.date(),
-          emails_to_invite: z.array(z.string().email()),
           owner_name: z.string(),
           owner_email: z.string().email(),
+          emails_to_invite: z.array(z.string().email()),
         }),
-        response: {
-          201: z.object({
-            tripId: z.string().uuid(),
-          }),
-          400: z.object({ message: z.string() }).describe('Bad request'),
-        },
       },
     },
-    async (request, reply) => {
+    async (request) => {
       const {
+        destination,
         starts_at,
         ends_at,
-        destination,
-        emails_to_invite,
         owner_name,
         owner_email,
+        emails_to_invite,
       } = request.body
 
       if (dayjs(starts_at).isBefore(new Date())) {
-        throw new Error('Invalid trip start date.')
+        throw new ClientError('Invalid trip start date.')
       }
 
       if (dayjs(ends_at).isBefore(starts_at)) {
-        throw new Error('Invalid trip end date.')
+        throw new ClientError('Invalid trip end date.')
       }
 
       const trip = await prisma.trip.create({
@@ -71,15 +64,12 @@ export const createTrip = async (app: FastifyInstance) => {
         },
       })
 
+      const formattedStartDate = dayjs(starts_at).format('LL')
+      const formattedEndDate = dayjs(ends_at).format('LL')
+
+      const confirmationLink = `${env.API_BASE_URL}/trips/${trip.id}/confirm`
+
       const mail = await getMailClient()
-
-      const formattedTripStartDate = dayjs(starts_at).format('D[ de ]MMMM')
-      const formattedTripEndDate = dayjs(ends_at).format('D[ de ]MMMM')
-
-      const confirmationLink = new URL(
-        `/trips/${trip.id}/confirm`,
-        env.API_BASE_URL,
-      )
 
       const message = await mail.sendMail({
         from: {
@@ -90,26 +80,25 @@ export const createTrip = async (app: FastifyInstance) => {
           name: owner_name,
           address: owner_email,
         },
-        subject: `Confirme sua viagem para ${destination} em ${formattedTripStartDate}`,
+        subject: `Confirme sua viagem para ${destination} em ${formattedStartDate}`,
         html: `
-          <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
-            <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de ${formattedTripStartDate} até ${formattedTripEndDate}.</p>
-            <p></p>
-            <p>Para confirmar sua viagem, clique no link abaixo:</p>
-            <p></p>
-            <p>
-              <a href="${confirmationLink.toString()}">Confirmar viagem</a>
-            </p>
-            <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
-          </div>
-        `.trim(),
+        <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+          <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong>.</p>
+          <p></p>
+          <p>Para confirmar sua viagem, clique no link abaixo:</p>
+          <p></p>
+          <p>
+            <a href="${confirmationLink}">Confirmar viagem</a>
+          </p>
+          <p></p>
+          <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
+        </div>
+      `.trim(),
       })
 
       console.log(nodemailer.getTestMessageUrl(message))
 
-      return reply.status(201).send({
-        tripId: trip.id,
-      })
+      return { tripId: trip.id }
     },
   )
 }
